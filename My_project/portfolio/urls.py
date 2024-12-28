8433a2f11113b96ffbf9e37425efcbb18c448e8f
Entue250 <edouardoniyomugabo@gmail.com>
2024-12-28 23:09:16 +0200

    Deploy static site

--- My_project/portfolio/urls.py
+++ My_project/portfolio/urls.py
@@ -2,8 +2,17 @@ from django.urls import path
 from django_distill import distill_path
 from . import views
 
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('about/', views.about, name='about'),
+#     path('contact/', views.contact, name='contact'),
+# ]
+
+def get_index():
+    return None
+
 urlpatterns = [
-    path('', distill_path(views.home, name='home')),
-    path('about/', distill_path(views.about, name='about')),
-    path('contact/', distill_path(views.contact, name='contact')),
+    distill_path('', views.home, name='home', distill_func=get_index),
+    distill_path('about/', views.about, name='about', distill_func=get_index),
+    distill_path('contact/', views.contact, name='contact', distill_func=get_index),
 ]
